22fe35c61ca24d894efedc64548fef921c9d6b73
Amirmohammad <amii_mhmd@yahoo.com>
2023-11-13 09:31:08 +0330

    script for input value

--- Day2_To-Do-List/script.js
+++ Day2_To-Do-List/script.js
@@ -1,1 +1,13 @@
-const inputBox = document.getElementById('input-box');
+const inputBox = document.getElementById('input-text');
+const listContainer = document.getElementById('list-container');
+
+addTask(){
+  if(inputBox === ""){
+    alert('You Must Write Something!');
+  }
+  else {
+    let li = document.createElement('li');
+    li.innerHTML = inputBox.value;
+    listContainer.appendChild(li);
+  }
+}
