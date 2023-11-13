e08457a6452cf2c93e0a7eefd826906342e1c3e3
Amirmohammad <amii_mhmd@yahoo.com>
2023-11-13 09:45:46 +0330

    style for created span

--- Day2_To-Do-List/script.js
+++ Day2_To-Do-List/script.js
@@ -1,13 +1,16 @@
 const inputBox = document.getElementById('input-text');
 const listContainer = document.getElementById('list-container');
 
-addTask(){
-  if(inputBox === ""){
+function addTask(){
+  if(inputBox.value === ""){
     alert('You Must Write Something!');
-  }
-  else {
+  }else {
     let li = document.createElement('li');
     li.innerHTML = inputBox.value;
     listContainer.appendChild(li);
+    let span = document.createElement("span");
+    span.innerHTML = "\u00d7";
+    li.appendChild(span);
   }
+  inputBox.value = "";
 }
