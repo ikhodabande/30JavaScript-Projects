be8cdef7009c0413427ee66f38adb76e38bb9c6b
Amirmohammad <amii_mhmd@yahoo.com>
2023-11-13 10:20:54 +0330

    completed , mobile size added

--- Day2_To-Do-List/script.js
+++ Day2_To-Do-List/script.js
@@ -15,14 +15,17 @@ function addTask(){
     li.appendChild(span);
   }
   inputBox.value = "";
+  saveData();
 }
 
 listContainer.addEventListener("click",(e)=>{
   if(e.target.tagName === "LI"){
     e.target.classList.toggle("checked");
+    saveData();
   }
   else if(e.target.tagName === "SPAN"){
     e.target.parentElement.remove();
+    saveData();
   }
 }, false);
 
@@ -30,5 +33,16 @@ listContainer.addEventListener("click",(e)=>{
 inputBox.addEventListener("keypress", (e)=>{
   if(e.key === "Enter"){
     addTask();
+    saveData();
   }
 } )
+
+function saveData(){
+  localStorage.setItem("data",listContainer.innerHTML);
+}
+
+function showTask(){
+  listContainer.innerHTML = localStorage.getItem("data");
+}
+
+showTask();
