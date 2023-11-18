0fce7af27f05426eb07b4cf8e6e5bf98d52eec9c
Amirmohammad <amii_mhmd@yahoo.com>
2023-11-18 13:02:53 +0330

    up

---
+++ Day3_Quiz-App/script.js
@@ -0,0 +1,11 @@
+const questions = [
+  {
+    question : 'which is largest animal in the world?'
+    ansewr : [
+      {text : 'Shark', correct : false},
+      {text : 'Blue-whale', correct : true},
+      {text : 'Elephant', correct : false},
+      {text : 'Girrafe', correct : false},
+    ]
+  }
+]
