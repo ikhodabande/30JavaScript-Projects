bbef6a6345c79bc9d7960f4eb57bfb7f92c70095
Amirmohammad <amii_mhmd@yahoo.com>
2023-11-20 09:33:46 +0330

    update quiz app

--- Day3_Quiz-App/script.js
+++ Day3_Quiz-App/script.js
@@ -1,11 +1,79 @@
 const questions = [
   {
-    question : 'which is largest animal in the world?'
-    ansewr : [
+    question : 'which is largest animal in the world?',
+    ansewrs : [
       {text : 'Shark', correct : false},
       {text : 'Blue-whale', correct : true},
       {text : 'Elephant', correct : false},
       {text : 'Girrafe', correct : false},
     ]
+  },
+  {
+    question : 'which is largest animal in the world?',
+    ansewrs : [
+      {text : 'Shark', correct : false},
+      {text : 'Blue-whale', correct : false},
+      {text : 'Elephant', correct : false},
+      {text : 'Girrafe', correct : true},
+    ]
+  },
+  {
+    question : 'which is largest animal in the world?',
+    ansewrs : [
+      {text : 'Shark', correct : true},
+      {text : 'Blue-whale', correct : false},
+      {text : 'Elephant', correct : false},
+      {text : 'Girrafe', correct : false},
+    ]
+  },
+  {
+    question : 'which is largest animal in the world?',
+    ansewrs : [
+      {text : 'Shark', correct : false},
+      {text : 'Blue-whale', correct : false},
+      {text : 'Elephant', correct : true},
+      {text : 'Girrafe', correct : false},
+    ]
+  },
+];
+
+const questionElement = document.getElementById("question") ;
+const ansewrButtons = document.getElementById("ansewr-btn") ;
+const nextButton = document.getElementById("next-btn") ;
+
+let currentQuestionIndex = 0;
+let score = 0 ;
+
+function startQuiz(){
+  currentQuestionIndex = 0;
+  score = 0 ;
+  nextButton.innerHTML = "Next";
+  showQuestion();
+}
+
+
+function showQuestion(){
+  resetState();
+  let currentQuestion = questions[currentQuestionIndex];
+  let questionNo = currentQuestionIndex + 1 ;
+  questionElement.innerHTML = questionNo + ". " + currentQuestion.question;
+
+  currentQuestion.ansewrs.forEach(ansewr => {
+    const button = document.createElement("button");
+    button.innerHTML = ansewr.text ;
+    button.classList.add("btn") ;
+    ansewrButtons.appendChild(button);
+     
+  });
+}
+
+
+function resetState(){
+  nextButton.style.display = "none";
+  while(ansewrButtons.firstChild){
+    ansewrButtons.removeChild(ansewrButtons.firstChild)
   }
-]
+}
+
+
+startQuiz();
