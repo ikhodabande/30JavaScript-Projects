11c242b1cbb00926e0d0e25c4ec47f8d1254840c
Amirmohammad <amii_mhmd@yahoo.com>
2024-01-07 23:54:29 +0330

    update js

--- Day5_Notes App/script.js
+++ Day5_Notes App/script.js
@@ -1,2 +1,25 @@
 const notesContainer = document.querySelector(".notes-container");
 const createBtn = document.querySelector(".btn");
+
+let notes = document.querySelectorAll(".input-box");
+
+createBtn.addEventListener("click",()=>{
+
+  let inputBox = document.createElement("p");
+  let inputImg = document.createElement("img");
+
+  inputBox.className = "input-box";
+  inputBox.setAttribute("contenteditable", "true");
+  inputImg.src = "./images/delete.png";
+
+  notesContainer.appendChild(inputBox).appendChild(inputImg)
+
+
+})
+
+
+notesContainer.addEventListener("click", (e)=>{
+  if(e.target.tagName === "IMG" ){
+    e.target.parentElement.remove();
+  }
+})
