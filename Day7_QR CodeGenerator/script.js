8e7da828575a2d83b6d9e184e616fc62d1905f32
Amirmohammad <amii_mhmd@yahoo.com>
2024-01-13 00:03:54 +0330

    css update

--- Day7_QR CodeGenerator/script.js
+++ Day7_QR CodeGenerator/script.js
@@ -1,3 +1,10 @@
+let imgBox = document.getElementById("imgBox");
+let qrImg = document.getElementById("qrImg");
+let qrText = document.getElementById("qrText");
+
+
+
 function generateQR(){
-  
+ qrImg.src = "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=" + qrText.value;
+
 }
