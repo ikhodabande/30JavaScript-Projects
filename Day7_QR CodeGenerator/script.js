dc3f6fd68768735d0c88e35b7f691cc636e2ae08
Amirmohammad <amii_mhmd@yahoo.com>
2024-01-13 00:25:24 +0330

    completed yesss

--- Day7_QR CodeGenerator/script.js
+++ Day7_QR CodeGenerator/script.js
@@ -5,6 +5,17 @@ let qrText = document.getElementById("qrText");
 
 
 function generateQR(){
- qrImg.src = "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=" + qrText.value;
+  if(qrText.value.length > 0){
+    qrImg.src = "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=" + qrText.value;
+    imgBox.classList.add("show-img");
+  }
+  else{
+    qrText.classList.add("error");
+    setTimeout(()=>{
+      qrText.classList.remove("error")
+    },1000);
 
+    
+  }
+ 
 }
