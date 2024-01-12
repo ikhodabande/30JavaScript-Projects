f03c144773cad3332da8ea3dc789bb5fb1882f9c
Amirmohammad <amii_mhmd@yahoo.com>
2024-01-12 16:15:57 +0330

    script update

--- Day6_Age Calculator/script.js
+++ Day6_Age Calculator/script.js
@@ -1,2 +1,20 @@
 let userInput = document.getElementById("date");
+userInput.max = new Date().toISOString().split("T")[0];
 
+
+function calculateAge(){
+
+  
+  let birthDate = new Date(userInput.value);
+
+  let d1 = birthDate.getDate();
+  let m1 = birthDate.getMonth();
+  let y1 = birthDate.getFullYear();
+
+  let today = new Date();
+
+  let d2 = today.getDate();
+  let m2 = today.getMonth();
+  let y2 = today.getFullYear();
+
+}
