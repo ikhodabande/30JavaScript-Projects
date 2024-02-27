4fd0f9d732053f500b0e47609b3fcdf092785650
Amirmohammad <amii_mhmd@yahoo.com>
2024-02-27 19:03:19 +0330

    add if and elses

---
+++ Day17-Select Menu/script.js
@@ -0,0 +1,12 @@
+
+var selectField = document.getElementsByClassName("selectField");
+var selectText = document.getElementById("selectText");
+var options = document.getElementsByClassName("options");
+
+console.log("enter");
+
+document.querySelector(".options").addEventListener("click",(e)=>{
+  console.log(e.target);
+});
+  
+
