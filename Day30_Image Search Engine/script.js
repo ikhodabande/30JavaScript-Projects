3f89cd1de998e34d3d4ebf445894ad8d302026b8
Amirmohammad <amii_mhmd@yahoo.com>
2024-04-27 15:48:02 +0330

    addign styles and some js syntax

---
+++ Day30_Image Search Engine/script.js
@@ -0,0 +1,4 @@
+const searchForm = document.getElementById("search-form");
+const searchBox = document.getElementById("search-box");
+const searchResult = document.getElementById("search-result");
+const searchBtn = document.getElementById("search-btn");
