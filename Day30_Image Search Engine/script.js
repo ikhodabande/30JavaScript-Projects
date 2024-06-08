68c0f2f77a512197848476f3e00844cc3585cb08
Amirmohammad <amii_mhmd@yahoo.com>
2024-06-09 00:44:46 +0330

    updated

--- Day30_Image Search Engine/script.js
+++ Day30_Image Search Engine/script.js
@@ -1,4 +1,4 @@
 const searchForm = document.getElementById("search-form");
 const searchBox = document.getElementById("search-box");
 const searchResult = document.getElementById("search-result");
-const searchBtn = document.getElementById("search-btn");
+const  searchBtn = document.getElementById("search-btn");
