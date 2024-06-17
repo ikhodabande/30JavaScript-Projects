5c9799cadb5cd8cc1c7098986906faf565e011f3
Amirmohammad <amii_mhmd@yahoo.com>
2024-06-17 14:03:22 +0330

    update project 30

--- Day30_Image Search Engine/script.js
+++ Day30_Image Search Engine/script.js
@@ -3,18 +3,44 @@ const accessKey = 'vTqEVQzkWPcq9HCEv7j90m5J_y14ntE5Y-4plq0QuK8';
 const searchForm = document.getElementById("search-form");
 const searchBox = document.getElementById("search-box");
 const searchResult = document.getElementById("search-result");
-const  searchBtn = document.getElementById("search-btn");
-
+const searchBtn = document.getElementById("show-more-btn");
 
 let keyboard = '';
 let page = 1;
 
 async function searchImage(){
   keyboard = searchBox.value;
-  const url = `https://api.unsplash.com/search/photos?page=${page}&query=${keyboard}&client_id=${accessKey}`
+  const url = `https://api.unsplash.com/search/photos?page=${page}&query=${keyboard}&client_id=${accessKey}`;
 
   const response = await fetch(url);
-  const date = await response.json();
+  const data = await response.json();
 
-  console.log(data)
-}
+  if(page === 1){
+    searchResult.innerHTML = "";
+  }
+
+  const results = data.results ;
+  results.map((result)=>{
+     const image = document.createElement("img");
+     image.src = result.urls.small;
+     const imageLink = document.createElement("a");
+     imageLink.href = result.links.html;
+     imageLink.target = "_blank";
+
+     imageLink.appendChild(image);
+     searchResult.appendChild(imageLink);
+  })
+
+  searchBtn.style.display = "block";
+};
+
+searchForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  page = 1;
+  searchImage();
+});
+
+searchBtn.addEventListener("click",()=>{
+  page++;
+  searchImage();
+})
