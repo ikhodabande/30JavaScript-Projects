378da2f997edaa38b7d5c8d597040ade01154df7
Amirmohammad <amii_mhmd@yahoo.com>
2024-07-01 22:20:55 +0330

    updated

--- javascript-in-react/src/components/Navbar.jsx
+++ javascript-in-react/src/components/Navbar.jsx
@@ -2,7 +2,14 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className='w-full h-[80px] px-2 flex items-center justify-between bg-[#f0db4f] text-[#323330]'>
+      <p className='font-extrabold text-2xl'>JavaScript</p>
+      <ul className='flex items-center '>
+        <li className='px-2 font-semibold'>Home</li>
+        <li className='px-2 font-semibold'>Contact</li>
+        <li className='px-2 font-semibold'>Projects</li>
+      </ul>
+    </div>
   )
 }
 
--- javascript-in-react/src/pages/Home.jsx
+++ javascript-in-react/src/pages/Home.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import Intro from '../components/Intro'
+import Navbar from '../components/Navbar'
 
 const Home = () => {
   return (
     <>
+    <Navbar/>
     <Intro/>
     </>
   )
